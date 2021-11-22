f3493e8989d368d9df1b13837c8c099f261438b3
NkosiMu <muzi.nkosi@kznhealth.gov.za>
2021-11-22 13:49:14 +0200

    Add project files.

---
+++ Complaint_Management_System/Data/Migrations/20211122095740_V1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Complaint_Management_System.Data.Migrations
+{
+    public partial class V1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
