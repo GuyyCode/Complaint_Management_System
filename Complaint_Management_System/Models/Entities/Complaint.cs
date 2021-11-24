b37f10c62c3af091c5966c5e7a82ed862d55eb18
NkosiMu <muzi.nkosi@kznhealth.gov.za>
2021-11-24 13:31:30 +0200

    Complaint Class

---
+++ Complaint_Management_System/Models/Entities/Complaint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Complaint_Management_System.Models.Entities
+{
+    public class Complaint
+    {
+        [Key]
+        public int ComplaintID { get; set; }
+
+        [Required]
+        [DisplayName("New Complaint")]
+        [StringLength(800)]
+        public string Description { get; set; }
+
+        public int StudentID { get; set; }
+
+    }
+}
